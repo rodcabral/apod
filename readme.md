3515e40df3df6193a848b9c33e862380cab971d6
Rodrigo Cabral <37848619+rodcabral@users.noreply.github.com>
2021-10-19 03:08:53 -0300

    First commit

---
+++ readme.md
@@ -0,0 +1,2 @@
+<h2 align="center">APOD</h2>
+<h3 align="center">Atronomy Picture of the Day</h3>
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { KEY } from "../config.js";
+
+const titleHtml = document.querySelector('.title');
+const dateHtml = document.querySelector('.date');
+const pictureOfTheDay = document.querySelector('.picture-of-day');
+const explanationHtml = document.querySelector('.explanation');
+const copyrightNameHtml = document.querySelector('.copyright-name');
+
+async function getPictureOfDay() {
+    const response = await axios.get(`https://api.nasa.gov/planetary/apod?api_key=${KEY}`);
+    const { copyright, hdurl, date, title, explanation } = await response.data;
+
+    titleHtml.innerText = title;
+    pictureOfTheDay.src = hdurl;
+    explanationHtml.innerText = explanation;
+    dateHtml.innerText = date
+    copyrightNameHtml.innerText = copyright || "nasa";
+}
+
+getPictureOfDay();
