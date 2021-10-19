81f8ffee22675314eca1091dcdb5f5e3c146ef4f
Rodrigo Cabral <37848619+rodcabral@users.noreply.github.com>
2021-10-19 17:24:19 -0300

    Refactoring

--- readme.md
+++ readme.md
@@ -1,2 +1,15 @@
 <h2 align="center">APOD</h2>
 <h3 align="center">Atronomy Picture of the Day</h3>
+
+<br/>
+<div align="center">
+    <img align="center" src="app.png">
+</div>
+<br/>
+
+_"Each day a different image or photograph of our fascinating universe is featured, along with a brief explanation written by a professional astronomer."_
+
+This project use the NASA API to show an Astronomy Picture of the day. 👩‍🚀👨‍🚀
+
+---
+Made with 💖 by [Rodrigo Cabral](https://www.linkedin.com/in/rodcabral)
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const dateHtml = document.querySelector('.date');
 const pictureOfTheDay = document.querySelector('.picture-of-day');
 const explanationHtml = document.querySelector('.explanation');
 const copyrightNameHtml = document.querySelector('.copyright-name');
+const pictureLink = document.querySelector('.picture-link');
 
 async function getPictureOfDay() {
     const response = await axios.get(`https://api.nasa.gov/planetary/apod?api_key=${KEY}`);
@@ -14,8 +15,10 @@ async function getPictureOfDay() {
     titleHtml.innerText = title;
     pictureOfTheDay.src = hdurl;
     explanationHtml.innerText = explanation;
-    dateHtml.innerText = date
+    dateHtml.innerText = date;
     copyrightNameHtml.innerText = copyright || "nasa";
+
+    pictureLink.href = hdurl
 }
 
 getPictureOfDay();
